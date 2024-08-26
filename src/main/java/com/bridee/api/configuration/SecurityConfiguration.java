0717ea641f2123dd5ed19b61503f8f0ff3e4e6a9
Ian Silva Santos <ian.santos@sptech.school>
2024-08-25 21:40:03 -0300

    feat: Liberando todas as rotas para o inicio do desenvolvimento.

---
+++ src/main/java/com/bridee/api/configuration/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.bridee.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(httpRequest -> {
+            httpRequest.anyRequest().permitAll();
+        }).build();
+    }
+
+}
