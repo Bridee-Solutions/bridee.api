90564a5d4c3bfdc3345f62a5d6127f6a3ed5b359
ElizeuVicente <elizeu.vicentefo@sptech.school>
2024-09-03 20:08:29 -0300

    Alterando migration table assessor_fornecedor

--- src/main/resources/db/migration/V202409021638__initial_schema.sql
+++ src/main/resources/db/migration/V202409021638__initial_schema.sql
@@ -13,3 +13,20 @@ alter table casal add column cep char(8);
 alter table casal add column estado_civil_parceiro varchar(45);
 alter table casal add column nome_parceiro varchar(45);
 
+create table assessor_fornecedor (
+    id int primary key auto_increment,
+    fk_fornecedor int,
+    fk_assessor int,
+    CONSTRAINT fkFornecedorAssessor FOREIGN KEY (fk_fornecedor) REFERENCES fornecedor (id),
+    CONSTRAINT fkAssessorFornecedor FOREIGN KEY (fk_assessor) REFERENCES assessor (id)
+);
+
+rename table convidado_evento to convite
+alter table acompanhante add column status varchar(45);
+alter table convite add column acompanhante_id int;
+alter table convite add constraint fkConviteAcompanhante FOREIGN KEY (acompanhante_id) REFERENCES acompanhante (id);
+
+
+alter table fornecedor drop column premium;
+alter table assessor add column premium boolean;
+
