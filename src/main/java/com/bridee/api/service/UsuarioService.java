4f16338626cfbaf627f0734488cf45d82c642511
ian15963 <ian.santos@sptech.school>
2024-09-05 17:47:39 -0300

    fix: Arrumando dependência do lombok e mapstruct.

--- src/main/java/com/bridee/api/service/UsuarioService.java
+++ src/main/java/com/bridee/api/service/UsuarioService.java
@@ -1,7 +1,8 @@
-package com.bridee.api.service;
+package com.bridee.api.services;
 
 import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.bridee.api.dto.request.UsuarioRequestDto;
@@ -19,28 +20,28 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UsuarioService {
     
-    private final UsuarioRepository repository;
-    private final UsuarioResponseMapper responseMapper;
-    private final UsuarioRequestMapper requestMapper;
-    
-    public UsuarioResponseDto resgister(UsuarioRequestDto usuarioRequestDto) {
-        Optional<Usuario> userEmail = this.repository.findByEmail(usuarioRequestDto.getEmail());
-        if (userEmail.isPresent()) throw new ResourceAlreadyExists();
-        Usuario user = this.requestMapper.toEntity(usuarioRequestDto);
-        return responseMapper.toDomain(this.repository.save(user));
-    }
-
-    public UsuarioResponseDto update(UsuarioRequestDto userRequestDto, Integer id){
-        if (!this.repository.existsById(id)) throw new ResourceNotFoundException();
-
-        userRequestDto.setId(id);
-        Usuario user = this.requestMapper.toEntity(userRequestDto);
-        return this.responseMapper.toDomain(this.repository.save(user));
-    }
-
-
-    public void deleteById(Integer id){
-        if (!repository.existsById(id)) throw new ResourceNotFoundException();
-        repository.deleteById(id);
-    }
+//    private final UsuarioRepository repository;
+//    private final UsuarioResponseMapper responseMapper;
+//    private final UsuarioRequestMapper requestMapper;
+//
+//    public UsuarioResponseDto resgister(UsuarioRequestDto usuarioRequestDto) {
+//        Optional<Usuario> userEmail = this.repository.findByEmail(usuarioRequestDto.getEmail());
+//        if (userEmail.isPresent()) throw new ResourceAlreadyExists();
+//        Usuario user = this.requestMapper.toEntity(usuarioRequestDto);
+//        return responseMapper.toDomain(this.repository.save(user));
+//    }
+//
+//    public UsuarioResponseDto put(UsuarioRequestDto userRequestDto, Integer id){
+//        if (!this.repository.existsById(id)) throw new ResourceNotFoundException();
+//
+//        userRequestDto.setId(id);
+//        Usuario user = this.requestMapper.toEntity(userRequestDto);
+//        return this.responseMapper.toDomain(this.repository.save(user));
+//    }
+//
+//
+//    public void deleteById(Integer id){
+//        if (!repository.existsById(id)) throw new ResourceNotFoundException();
+//        repository.deleteById(id);
+//    }
 }
