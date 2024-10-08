a4daaf0968ad29405a671dc87634f2947f5cb411
Ian Silva Santos <ian.santos@sptech.school>
2024-10-08 20:36:13 -0300

    feat: Configurando o DBUnit com o DBRider.

--- src/test/java/com/bridee/api/ApplicationTests.java
+++ src/test/java/com/bridee/api/ApplicationTests.java
@@ -1,9 +1,13 @@
 package com.bridee.api;
 
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DBRider
+@DBUnit(leakHunter = true)
 class ApplicationTests {
 
 	@Test
