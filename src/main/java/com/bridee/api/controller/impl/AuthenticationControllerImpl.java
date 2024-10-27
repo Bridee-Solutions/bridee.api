9bda8995cffe7f3a8e3f8e58250941eb59057c2c
Ian Silva Santos <ian.santos@sptech.school>
2024-10-27 14:24:42 -0300

    feat: Adicionando domain cookie nas variáveis de ambiente.

--- src/main/java/com/bridee/api/controller/impl/AuthenticationControllerImpl.java
+++ src/main/java/com/bridee/api/controller/impl/AuthenticationControllerImpl.java
@@ -7,6 +7,7 @@ import com.bridee.api.service.AuthenticationService;
 import com.bridee.api.utils.CookieUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -24,13 +25,15 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public class AuthenticationControllerImpl implements AuthenticationController {
 
+    @Value("${httpOnly.cookie.host}")
+    private String host;
     private final AuthenticationService authenticationService;
 
     @PostMapping("/authentication")
     public ResponseEntity<AuthenticationResponseDto> authenticate(@RequestBody @Valid AuthenticationRequestDto authenticationRequest){
         AuthenticationResponseDto authenticationResponse = authenticationService.authenticate(authenticationRequest);
-        HttpCookie accessTokenCookie = CookieUtils.createCookie("access_token", authenticationResponse.getAccessToken(), true, Duration.ofHours(1),"localhost");
-        HttpCookie refreshTokenCookie = CookieUtils.createCookie("refresh_token", authenticationResponse.getRefreshToken(), true, Duration.ofDays(7), "localhost");
+        HttpCookie accessTokenCookie = CookieUtils.createCookie("access_token", authenticationResponse.getAccessToken(), true, Duration.ofHours(1), host);
+        HttpCookie refreshTokenCookie = CookieUtils.createCookie("refresh_token", authenticationResponse.getRefreshToken(), true, Duration.ofDays(7), host);
         MultiValueMap<String, String> headers = new HttpHeaders();
         headers.add(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
         headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
