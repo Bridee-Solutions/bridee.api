c16cb8b4eaa827baff27f34b0aae8c5d7e6d5edb
ian15963 <ian.santos@sptech.school>
2025-01-04 16:46:42 -0300

    feat: Adicionando client para se comunicar com a função servless de upload e download de arquivos.

---
+++ src/main/java/com/bridee/api/client/AzureStorageFunctionClient.java
@@ -0,0 +1,20 @@
+package com.bridee.api.client;
+
+import com.bridee.api.client.dto.request.FileRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "blobStorageFunction", url = "${services.bridee.function.storage-function.url}")
+public interface AzureStorageFunctionClient {
+
+    @PostMapping("/uploadFile")
+    ResponseEntity<Void> uploadFile(@RequestBody FileRequest request,
+                                    @RequestHeader("x-api-key") String apiKey);
+
+    ResponseEntity<byte[]> downloadFile(@RequestBody FileRequest request,
+                                        @RequestHeader("x-api-key") String apiKey);
+}
