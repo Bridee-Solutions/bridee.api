992447da73b6396d5ebc2aaaeb84634acf8a20d1
ian15963 <ian.santos@sptech.school>
2025-01-12 16:21:19 -0300

    feat: Usando JsonMergePatch na controller de casal.

--- src/main/java/com/bridee/api/controller/impl/CasalControllerImpl.java
+++ src/main/java/com/bridee/api/controller/impl/CasalControllerImpl.java
@@ -17,6 +17,7 @@ import com.bridee.api.pattern.strategy.blobstorage.impl.AzureBlobStorageImpl;
 import com.bridee.api.service.CasalService;
 import com.bridee.api.service.CasamentoService;
 import com.bridee.api.service.ImagemCasalService;
+import com.bridee.api.utils.PatchHelper;
 import com.bridee.api.utils.UriUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,17 +27,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.json.JsonMergePatch;
 import java.math.BigDecimal;
 
 @RestController
@@ -51,6 +45,7 @@ public class CasalControllerImpl implements CasalController {
     private final CasalExternoResponseMapper externoResponseMapper;
     private final CasamentoService casamentoService;
     private final ImagemCasalService imagemCasalService;
+    private final PatchHelper patchHelper;
 
     @GetMapping
     public ResponseEntity<Page<CasalResponseDto>> findAll(Pageable pageable){
@@ -94,9 +89,10 @@ public class CasalControllerImpl implements CasalController {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<CasalResponseDto> update(@RequestBody @Valid CasalRequestDto requestDto, @PathVariable Integer id){
-        Casal casal = requestMapper.toEntity(requestDto);
+    @PatchMapping(value = "/{id}", consumes = "application/merge-patch+json")
+    public ResponseEntity<CasalResponseDto> update(@RequestBody JsonMergePatch jsonMergePatch, @PathVariable Integer id){
+//        Casal casal = requestMapper.toEntity(requestDto);
+        Casal casal = patchHelper.mergePatch(jsonMergePatch, new Casal(), Casal.class);
         CasalResponseDto responseDto = responseMapper.toDomain(service.update(casal, id));
         return ResponseEntity.ok(responseDto);
     }
