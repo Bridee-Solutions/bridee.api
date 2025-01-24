14e0ef1c5d38508dfebe15bf668edc54abaf6269
ian15963 <ian.santos@sptech.school>
2025-01-24 15:37:38 -0300

    feat: Criando endpoint para cadastro de cronograma e ajuste no check do cadastro de atividades.

---
+++ src/main/java/com/bridee/api/controller/impl/CronogramaControllerImpl.java
@@ -0,0 +1,28 @@
+package com.bridee.api.controller.impl;
+
+import com.bridee.api.dto.request.CronogramaRequestDto;
+import com.bridee.api.entity.Cronograma;
+import com.bridee.api.mapper.request.CronogramaMapper;
+import com.bridee.api.service.CronogramaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cronogramas")
+@RequiredArgsConstructor
+public class CronogramaControllerImpl {
+
+    private final CronogramaService service;
+    private final CronogramaMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<Cronograma> save(@RequestBody CronogramaRequestDto request){
+        Cronograma cronograma = mapper.toEntity(request);
+        return ResponseEntity.ok(service.save(cronograma));
+    }
+
+}
