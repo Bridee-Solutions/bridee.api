c8aecc4eb5243a0c79b88721849abbbdfa642159
ian15963 <ian.santos@sptech.school>
2025-01-24 15:42:57 -0300

    fix: Removendo uso da pilha e validação desnecessária

--- src/main/java/com/bridee/api/service/PedidoAssessoriaService.java
+++ src/main/java/com/bridee/api/service/PedidoAssessoriaService.java
@@ -40,14 +40,11 @@ public class PedidoAssessoriaService {
             return pedidoAssessorado;
         }
         var pedidosPendentes = repository.findAllPedidosCasamentoByStatus(casamentoId, PedidoAssessoriaStatusEnum.PENDENTE_APROVACAO);
-        if (Objects.nonNull(pedidosPendentes) && !pedidosPendentes.isEmpty()){
-            pedidosPendentes.sort(Comparator.comparing(PedidoAssessoria::getId).reversed());
-        }
-      
         if (pedidosPendentes.isEmpty()){
             return null;
         }
-      
+        pedidosPendentes.sort(Comparator.comparing(PedidoAssessoria::getId).reversed());
+
         return pedidosPendentes.get(0);
     }
 
@@ -136,13 +133,7 @@ public class PedidoAssessoriaService {
 
     public void invalidateWeddings(){
         List<PedidoAssessoria> allPedidosCasamentoInvalido = repository.findAllPedidosCasamentoInvalido();
-        Stack<PedidoAssessoria> pedidoAssessoriaStack = new Stack<>();
-        allPedidosCasamentoInvalido.forEach(pedidoAssessoriaStack::push);
-        while (!pedidoAssessoriaStack.isEmpty() && pedidoAssessoriaStack.peek() != null){
-            PedidoAssessoria pedidoAssessoria = pedidoAssessoriaStack.pop();
-            pedidoAssessoria.setStatus(PedidoAssessoriaStatusEnum.INVALIDO);
-        }
-        allPedidosCasamentoInvalido = pedidoAssessoriaStack.stream().toList();
+        allPedidosCasamentoInvalido.forEach(pedido -> pedido.setStatus(PedidoAssessoriaStatusEnum.INVALIDO));
         repository.saveAll(allPedidosCasamentoInvalido);
     }
 }
