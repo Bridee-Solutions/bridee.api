233bf82e8a90c05b94aaf6764cfdb6552f15bf8c
ian15963 <ian.santos@sptech.school>
2025-02-02 08:45:06 -0300

    feat: Adicionando cache na busca pelas subcategorias.

--- src/main/java/com/bridee/api/service/SubcategoriaServicoService.java
+++ src/main/java/com/bridee/api/service/SubcategoriaServicoService.java
@@ -1,11 +1,14 @@
 package com.bridee.api.service;
 
+import com.bridee.api.configuration.cache.CacheConstants;
 import com.bridee.api.entity.SubcategoriaServico;
 import com.bridee.api.exception.ResourceNotFoundException;
 import com.bridee.api.repository.projection.orcamento.SubcategoriaProjection;
 import com.bridee.api.repository.FornecedorRepository;
 import com.bridee.api.repository.SubcategoriaServicoRepository;
+import com.bridee.api.utils.PageUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -21,20 +24,29 @@ public class SubcategoriaServicoService {
     private final SubcategoriaServicoRepository subcategoriaServicoRepository;
     private final CategoriaServicoService categoriaServicoService;
     private final FornecedorRepository fornecedorRepository;
+    private final SubcategoriaServicoServiceCacheAssistent cacheAssistent;
 
-    public List<SubcategoriaProjection> findAll(){
-        return subcategoriaServicoRepository.findAllProjections();
+    public List<SubcategoriaServico> findAll(){
+        return cacheAssistent.findAll();
     }
 
     @Transactional(readOnly = true)
     public Page<SubcategoriaServico> findAllByCategoria(Integer categoriaId, Pageable pageable) {
         categoriaServicoService.existsById(categoriaId);
-        return subcategoriaServicoRepository.findAllByCategoriaId(categoriaId, pageable);
+        List<SubcategoriaServico> subcategorias = cacheAssistent.findAll()
+                .stream()
+                .filter(subcategoria ->
+                        subcategoria.getCategoriaServico().getId().equals(categoriaId)).toList();
+
+        return PageUtils.collectionToPage(subcategorias, pageable);
     }
 
     public List<SubcategoriaServico> findAllByCategoria(Integer categoriaId){
         categoriaServicoService.existsById(categoriaId);
-        return subcategoriaServicoRepository.findAllByCategoriaId(categoriaId);
+        return cacheAssistent.findAll()
+                .stream()
+                .filter(subcategoria ->
+                        subcategoria.getCategoriaServico().getId().equals(categoriaId)).toList();
     }
 
     public void existsById(Integer id){
@@ -52,6 +64,10 @@ public class SubcategoriaServicoService {
 
     @Transactional(readOnly = true)
     public Page<SubcategoriaServico> findAllByCategoriaName(String nome, Pageable pageable) {
-        return subcategoriaServicoRepository.findAllByCategoriaNome(nome, pageable);
+        List<SubcategoriaServico> subcategorias = cacheAssistent.findAll().stream()
+                .filter(subcategoria -> subcategoria.getCategoriaServico().getNome().equals(nome))
+                .toList();
+
+        return PageUtils.collectionToPage(subcategorias, pageable);
     }
 }
