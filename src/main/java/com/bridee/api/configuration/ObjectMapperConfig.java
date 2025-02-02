1779510305757fc21f5bd3c485ac38f1d7a4b433
ian15963 <ian.santos@sptech.school>
2025-02-02 10:48:47 -0300

    refactor: Centralizando configuração do object mapper.

--- src/main/java/com/bridee/api/configuration/ObjectMapperConfig.java
+++ src/main/java/com/bridee/api/configuration/ObjectMapperConfig.java
@@ -1,6 +1,11 @@
 package com.bridee.api.configuration;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +14,13 @@ public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+        return JsonMapper.builder()
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .findAndAddModules()
+                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                .build();
     }
 
 }
