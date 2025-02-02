79ad2ee25365f93a0c2a3f0599c6e4005e1e5944
ian15963 <ian.santos@sptech.school>
2025-02-02 08:39:11 -0300

    feat: Adicionando cache na busca pelas categorias.

--- src/main/java/com/bridee/api/service/CategoriaServicoService.java
+++ src/main/java/com/bridee/api/service/CategoriaServicoService.java
@@ -1,9 +1,11 @@
 package com.bridee.api.service;
 
+import com.bridee.api.configuration.cache.CacheConstants;
 import com.bridee.api.entity.CategoriaServico;
 import com.bridee.api.exception.ResourceNotFoundException;
 import com.bridee.api.repository.CategoriaServicoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ public class CategoriaServicoService {
 
     private final CategoriaServicoRepository repository;
 
+    @Cacheable(cacheNames = CacheConstants.CATEGORIA_SERVICO)
     public Page<CategoriaServico> findAll(Pageable pageable){
         return repository.findAll(pageable);
     }
