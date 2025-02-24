115b27de2f92bc2ffb9eda6b45c0970e442e557e
Ian Silva Santos <ian.santos@sptech.school>
2025-02-24 17:07:37 -0300

    feat: Adicionando logs de cronograma.

--- src/main/java/com/bridee/api/controller/impl/CronogramaControllerImpl.java
+++ src/main/java/com/bridee/api/controller/impl/CronogramaControllerImpl.java
@@ -6,12 +6,14 @@ import com.bridee.api.mapper.request.CronogramaMapper;
 import com.bridee.api.service.CronogramaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/cronogramas")
 @RequiredArgsConstructor
@@ -22,6 +24,7 @@ public class CronogramaControllerImpl {
 
     @PostMapping
     public ResponseEntity<Cronograma> save(@RequestBody @Valid CronogramaRequestDto request){
+        log.info("CRONOGRAMA: persistindo o cronograma e suas atividades");
         Cronograma cronograma = mapper.toEntity(request);
         return ResponseEntity.ok(service.save(cronograma));
     }
