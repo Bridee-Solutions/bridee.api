f3cef79aa7ceaa469c075e5dc780b285266b42ab
ian15963 <ian.santos@sptech.school>
2025-03-03 18:45:18 -0300

    feat: Adicionando log de subcategoria e tarefa.

--- src/main/java/com/bridee/api/controller/impl/SubcategoriaServicoControllerImpl.java
+++ src/main/java/com/bridee/api/controller/impl/SubcategoriaServicoControllerImpl.java
@@ -6,6 +6,7 @@ import com.bridee.api.entity.SubcategoriaServico;
 import com.bridee.api.mapper.response.SubcategoriaServicoResponseMapper;
 import com.bridee.api.service.SubcategoriaServicoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/subcategorias")
 @RequiredArgsConstructor
@@ -25,6 +27,7 @@ public class SubcategoriaServicoControllerImpl implements SubcategoriaServicoCon
 
     @GetMapping("/categoria/{categoriaId}")
     public ResponseEntity<Page<SubcategoriaServicoResponseDto>> findAllByCategoria(@PathVariable Integer categoriaId,  Pageable pageable){
+        log.info("SUBCATEGORIA SERVICO: buscando subcategorias relacionadas a categoria de id {}", categoriaId);
         Page<SubcategoriaServico> subcategoriaServicos = subcategoriaServicoService.findAllByCategoria(categoriaId, pageable);
         Page<SubcategoriaServicoResponseDto> responseDtoPage = responseMapper.toDomain(subcategoriaServicos);
         return ResponseEntity.ok(responseDtoPage);
@@ -32,6 +35,7 @@ public class SubcategoriaServicoControllerImpl implements SubcategoriaServicoCon
 
     @GetMapping("/categoria/nome/{nome}")
     public ResponseEntity<Page<SubcategoriaServicoResponseDto>> findAllByCategoriaName(@PathVariable String nome, Pageable pageable){
+        log.info("SUBCATEGORIA SERVICO: buscando subcategorias relacionadas a categoria de nome {}", nome);
         Page<SubcategoriaServico> subcategorias = subcategoriaServicoService.findAllByCategoriaName(nome, pageable);
         Page<SubcategoriaServicoResponseDto> response = responseMapper.toDomain(subcategorias);
         return ResponseEntity.ok(response);
