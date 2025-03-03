2245cfdb9d777375f47f8ab3bcc92880c170127e
ian15963 <ian.santos@sptech.school>
2025-03-03 16:59:59 -0300

    feat: Adicionando log em item orcamento e custos

--- src/main/java/com/bridee/api/service/CustoService.java
+++ src/main/java/com/bridee/api/service/CustoService.java
@@ -6,11 +6,13 @@ import com.bridee.api.exception.ResourceNotFoundException;
 import com.bridee.api.repository.CustoRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustoService {
@@ -20,6 +22,7 @@ public class CustoService {
     @Transactional
     public Custo save(Custo custo){
         validateCusto(custo);
+        log.info("CUSTO: criando custo {}", custo.getNome());
         return custoRepository.save(custo);
     }
 
@@ -27,15 +30,18 @@ public class CustoService {
     private void validateCusto(Custo custo){
         Integer itemOrcamentoId = Objects.nonNull(custo.getItemOrcamento()) ? custo.getItemOrcamento().getId() : null;
         if (itemOrcamentoId == null){
+            log.error("CUSTO: custo não vinculado a nenhum item");
             throw new IllegalArgumentException("Custo não vinculado a um item!");
         }
         if (custoRepository.existsByNomeAndItemOrcamentoId(custo.getNome(), itemOrcamentoId)){
+            log.error("CUSTO: custo já cadastrado com nome {}, para o item orçamento {}", custo.getNome(), itemOrcamentoId);
             throw new ResourceAlreadyExists("Custo já cadastro ao item orçamento!");
         }
     }
 
     @Transactional
     public List<Custo> saveAll(List<Custo> custos){
+        log.info("CUSTO: salvando {} custos", custos.size());
         return custoRepository.saveAll(custos);
     }
 
