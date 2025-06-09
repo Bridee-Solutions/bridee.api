71b02e8f49f19096e4bfcef6681edc3d891c585a
ian15963 <ian.santos@sptech.school>
2025-06-09 00:07:25 -0300

    feat: Adicionando access token na resposta do refresh.

--- src/main/java/com/bridee/api/controller/impl/AuthenticationControllerImpl.java
+++ src/main/java/com/bridee/api/controller/impl/AuthenticationControllerImpl.java
@@ -3,6 +3,7 @@ package com.bridee.api.controller.impl;
 import com.bridee.api.controller.AuthenticationController;
 import com.bridee.api.dto.request.AuthenticationRequestDto;
 import com.bridee.api.dto.response.AuthenticationResponseDto;
+import com.bridee.api.dto.response.RefreshTokenResponse;
 import com.bridee.api.service.AuthenticationService;
 import com.bridee.api.utils.CookieUtils;
 import jakarta.servlet.http.HttpServletRequest;
@@ -44,14 +45,14 @@ public class AuthenticationControllerImpl implements AuthenticationController {
     }
 
     @PostMapping("/refresh-token")
-    public ResponseEntity<Void> refreshToken(HttpServletRequest request){
+    public ResponseEntity<RefreshTokenResponse> refreshToken(HttpServletRequest request){
         String refreshToken = authenticationService.extractRefreshTokenFromRequest(request);
         String accessToken = authenticationService.generateNewAccessToken(refreshToken);
 
         HttpCookie accessTokenCookie = CookieUtils.createCookie("access_token", accessToken, true, Duration.ofHours(1), host);
         MultiValueMap<String, String> headers = new HttpHeaders();
         headers.add(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
-        return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(new RefreshTokenResponse(accessToken), headers, HttpStatus.OK);
     }
 
 }
