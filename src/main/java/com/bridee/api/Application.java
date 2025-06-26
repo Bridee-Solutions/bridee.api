8bcdc699c42c68706aad31b03641a99e957ac604
ian15963 <ian.santos@sptech.school>
2025-06-25 21:22:25 -0300

    fix: Atualizando versões no pom.xml

--- src/main/java/com/bridee/api/Application.java
+++ src/main/java/com/bridee/api/Application.java
@@ -12,8 +12,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
